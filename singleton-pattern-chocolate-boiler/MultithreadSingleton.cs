0cc8603b03ee42120273d31337628982b51bc2ba
Senthil Elayappan <elayas1@sigi.us.selective.com>
2021-09-02 15:04:39 -0400

    added the volatile keyword infomration

--- singleton-pattern-chocolate-boiler/MultithreadSingleton.cs
+++ singleton-pattern-chocolate-boiler/MultithreadSingleton.cs
@@ -10,6 +10,27 @@ namespace singleton_pattern_chocolate_boiler
      */
     public class MultithreadSingleton
     {
+        /* volatile - When your program executes, the processor may cache the data and then access this data from the cache 
+         * when it is requested by the executing thread. Updates and reads of this data might run against the cached version of the data, 
+         * while the main memory is updated at a later point in time. This model of memory usage has consequences for multithreaded applications. 
+         * When one thread is interacting with the data in the cache, and a second thread tries to read the same data concurrently,
+         * the second thread might read an outdated version of the data from the main memory. 
+         * This is because when the value of a non-volatile object is updated, 
+         * the change is made in the cache of the executing thread and not in the main memory.
+         * 
+         * When you mark an object or a variable as volatile, it becomes a candidate for volatile reads and writes. 
+         * It should be noted that in C# all memory writes are volatile irrespective of whether you are writing data to a volatile or a non-volatile object.
+         * However, the ambiguity happens when you are reading data. When you are reading data that is non-volatile, 
+         * the executing thread may or may not always get the latest value. 
+         * If the object is volatile, the thread always gets the most up-to-date value.
+         * 
+         * You can use the volatile keyword with any reference, pointer, and enum types. 
+         * You can also use the volatile modifier with byte, short, int, char, float, and bool types. 
+         * It should be noted that local variables cannot be declared as volatile. 
+         * 
+         *  Also, a double variable cannot be volatile because it is 64 bits in size, larger than the word size on x86 systems. 
+         *  If you need to make a double variable volatile, you should wrap it inside in class. 
+         */
         private volatile static MultithreadSingleton uniqueInstance = null;
         private readonly static object myLocker = new object();
 
